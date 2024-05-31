b3e27e2973768d566c49a941a3674af7df303fef
Igor Białas <igor.bialas0@gmail.com>
2024-05-31 16:44:14 +0200

    14. Zmiana klasy node na prywatną i reorganizacja projektu

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -38,4 +38,35 @@ public class IntLinkedList {
         last = last.getPrev();
         return ret;
     }
+
+    private class Node {
+        private int value;
+        private Node prev;
+        private Node next;
+
+        Node(int i) {
+            value = i;
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        Node getPrev() {
+            return prev;
+        }
+
+        void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        Node getNext() {
+            return next;
+        }
+
+        void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
 }
